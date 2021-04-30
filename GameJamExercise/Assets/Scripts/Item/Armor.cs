f8c56584aec5eba57bcf007d14ad71eb20d408b7
Oscar Dao <43139435+oscardao@users.noreply.github.com>
2021-04-30 23:21:14 +0200

    as

---
+++ GameJamExercise/Assets/Scripts/Item/Armor.cs
@@ -0,0 +1,14 @@
+﻿using OsukaCreative.Utility.Variables;
+using System.Collections;
+using UnityEngine;
+
+
+public class Armor : MonoBehaviour, IInteractable {
+
+    [SerializeField]
+    private BoolVariable playerArmor;
+
+    public void OnInteract(GameObject interacter) {
+
+    }
+}
---
+++ GameJamExercise/Assets/Scripts/Item/Sword.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Sword : MonoBehaviour, IInteractable {
+
+    public void OnInteract(GameObject interacter) {
+
+    }
+
+}
